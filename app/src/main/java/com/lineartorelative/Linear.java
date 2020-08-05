f82b38d78ebff63a5fd9a858f43956d895054944
Bama Qyandija Deandra <bamaqyan@gmail.com>
2020-08-05 15:20:18 +0700

    kesatu 1

--- app/src/main/java/com/lineartorelative/Linear.java
+++ app/src/main/java/com/lineartorelative/Linear.java
@@ -2,13 +2,32 @@ package com.lineartorelative;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Linear extends AppCompatActivity {
+public class Linear extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear);
+
+        Button btnrelative = findViewById(R.id.btn_daftar);
+        btnrelative.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_daftar:
+                String phoneNumber = "085708339519";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/lineartorelative/MainActivity.java
+++ app/src/main/java/com/lineartorelative/MainActivity.java
@@ -2,13 +2,35 @@ package com.lineartorelative;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnrelative = findViewById(R.id.btnrelative);
+        btnrelative.setOnClickListener(this);
+
+        Button btnreal = findViewById(R.id.btnreal);
+        btnreal.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnrelative:
+                Intent move = new Intent(MainActivity.this, Relative.class);
+                startActivity(move);
+                break;
+            case R.id.btnreal:
+                Intent intent = new Intent(MainActivity.this, Linear.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
